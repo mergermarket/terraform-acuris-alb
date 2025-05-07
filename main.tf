daafe50600beeffc0c156cbcf522ef61a2b215e3
Keir Badger <keir.badger@iongroup.com>
2025-05-07 06:09:29 +0000

    add preserve_host_header

--- main.tf
+++ main.tf
@@ -1,17 +1,18 @@
 data "aws_acm_certificate" "cert" {
-  count    = var.run_data ? 1 : 0
-  domain   = var.certificate_domain_name
-  statuses = ["ISSUED"]
+  count     = var.run_data ? 1 : 0
+  domain    = var.certificate_domain_name
+  statuses  = ["ISSUED"]
   key_types = ["RSA_4096", "RSA_2048"]
 }
 
 resource "aws_alb" "alb" {
-  name            = replace(replace(var.name, "/(.{0,32}).*/", "$1"), "/^-+|-+$/", "")
-  internal        = var.internal
-  security_groups = concat([aws_security_group.default.id], var.extra_security_groups)
-  subnets         = var.subnet_ids
-  tags            = var.tags
-  idle_timeout    = var.idle_timeout
+  name                 = replace(replace(var.name, "/(.{0,32}).*/", "$1"), "/^-+|-+$/", "")
+  internal             = var.internal
+  security_groups      = concat([aws_security_group.default.id], var.extra_security_groups)
+  subnets              = var.subnet_ids
+  tags                 = var.tags
+  idle_timeout         = var.idle_timeout
+  preserve_host_header = var.preserve_host_header
 
   access_logs {
     bucket  = var.access_logs_bucket
