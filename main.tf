a6295eac74b412d1ee2f0c441b0b0ad3a323e3fb
Marcio Goda <marciogoda@gmail.com>
2025-06-20 16:25:44 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ data "aws_acm_certificate" "cert" {
 }
 
 resource "aws_alb" "alb" {
-  name                 = replace(replace(var.name, "/(.{0,32}).*/", "$1"), "/^-+|-+$/", "")
+  name                 = replace(replace(replace(var.name, "/(.{0,32}).*/", "$1"), "/^-+|-+$/", ""),"_","-")
   internal             = var.internal
   security_groups      = concat([aws_security_group.default.id], var.extra_security_groups)
   subnets              = var.subnet_ids
